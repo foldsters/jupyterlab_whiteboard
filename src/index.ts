c33eba002f9d212b8592a3f4709fe3764369d380
Foldster <foldsters@gmail.com>
2020-01-28 10:46:20 -0500

    Show whiteboard in palette

--- src/index.ts
+++ src/index.ts
@@ -2,15 +2,47 @@ import {
   JupyterFrontEnd, JupyterFrontEndPlugin
 } from '@jupyterlab/application';
 
+import {
+  ICommandPalette, MainAreaWidget
+} from '@jupyterlab/apputils';
+
+import {
+  Widget
+} from '@phosphor/widgets';
 
 /**
- * Initialization data for the jupyterlab_whiteboard extension.
+ * Initialization data for the jupyterlab_apod extension.
  */
 const extension: JupyterFrontEndPlugin<void> = {
-  id: 'jupyterlab_whiteboard',
+  id: 'jupyterlab_wb',
   autoStart: true,
-  activate: (app: JupyterFrontEnd) => {
-    console.log('JupyterLab extension jupyterlab_whiteboard is activated!');
+  requires: [ICommandPalette],
+  activate: (app: JupyterFrontEnd, palette: ICommandPalette) => {
+    console.log('JupyterLab extension jupyterlab_wb is activated!');
+
+    // Create a blank content widget inside of a MainAreaWidget
+    const content = new Widget();
+    const widget = new MainAreaWidget({content});
+    widget.id = 'wb-jupyterlab';
+    widget.title.label = 'Whiteboard';
+    widget.title.closable = true;
+
+    // Add an application command
+    const command: string = 'wb:open';
+    app.commands.addCommand(command, {
+      label: 'Whiteboard',
+      execute: () => {
+        if (!widget.isAttached) {
+          // Attach the widget to the main work area if it's not there
+          app.shell.add(widget, 'main');
+        }
+        // Activate the widget
+        app.shell.activateById(widget.id);
+      }
+    });
+
+    // Add the command to the palette.
+    palette.addItem({command, category: 'Tutorial'});
   }
 };
 
